59b7f2234796fe65cac503a3b6b6841796ce72af
Matthew Short <short.matthew.f@gmail.com>
2014-05-22 17:50:09 -0400

    Second commit

--- lib/active_record_lite/02_sql_object.rb
+++ lib/active_record_lite/02_sql_object.rb
@@ -10,15 +10,42 @@ end
 
 class SQLObject < MassObject
   def self.columns
-    # ...
+    col_names = DBConnection.execute2("SELECT * FROM cats").first
+    col_names.each do |name|
+      define_method("#{name}") do
+        self.instance_variable_get("@#{name}")
+      end
+      
+      define_method("#{name}=") do |value|
+        self.instance_variable_set("@#{name}", value)
+      end      
+    end
+    col_names
   end
 
   def self.table_name=(table_name)
-    # ...
+    self.instance_variable_set("@table_name", table_name)
   end
 
   def self.table_name
-    # ...
+    name = self.instance_variable_get("@table_name")
+    if name
+      name
+    else
+      self.to_s.tableize
+    end
+  end
+
+  def self.parse_all(hashes)
+    objects = []
+    hashes.each do |hash|
+      current = self.new
+      hash.each do |k, v|
+        current.instance_variable_set("@#{k}", "#{v}")
+      end
+      objects << current
+    end
+    objects
   end
 
   def self.all
@@ -30,15 +57,20 @@ class SQLObject < MassObject
   end
 
   def attributes
-    # ...
+    current = self.instance_variable_get("@attributes") || 
+      self.instance_variable_set("@attributes", Hash.new)
+
+    current
   end
 
   def insert
     # ...
   end
 
-  def initialize
-    # ...
+  def initialize(params)
+    params.each do |attr_name, value|
+      
+    end
   end
 
   def save
@@ -50,6 +82,8 @@ class SQLObject < MassObject
   end
 
   def attribute_values
-    # ...
+    current = self.instance_variable_get("@attributes")
+    
+    current.values
   end
 end
